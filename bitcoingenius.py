3a4b44db9a766888b28257b73fcaa2922b122d07
Svapnil Ankolkar <svankolk@ncsu.edu>
2017-12-29 12:45:49 -0500

    added code to allow seamless restarting of script

--- bitcoingenius.py
+++ bitcoingenius.py
@@ -14,6 +14,17 @@ reddit = praw.Reddit(client_id= ACCOUNTPRIVATEINFO.REDDIT_CLIENT_ID,
                      user_agent= ACCOUNTPRIVATEINFO.REDDIT_USER_AGENT)
 
 usedComments = set()
+
+#load recent already posted tweets into set
+postedStatuses = tweepy.Cursor(api.user_timeline).items(100)
+for status in postedStatuses:
+    usedComments.add(status.text[:100])
+
+
+#there is a problem where tweepy status.text will output a concatonated version of the target
+#which is an issue for the set that holds individual tweets. To fix, i will only add the first
+#100 characters of each tweet to the set to make sure i can properly identify duplicates
+
 count = 0
 
 #loop for the code in the Script
@@ -24,18 +35,18 @@ while(True):
             firstComment = submission.comments[0].body
         except IndexError as ie:
             print(submission.title + ' doesnt have a comment - skipping')
-            continue   
+            continue
 
-        if firstComment in usedComments or len(firstComment) > 280:
+        if firstComment[:100] in usedComments or len(firstComment) > 280:
             continue
         else:
             api.update_status(firstComment)
             print(firstComment)
-            usedComments.add(firstComment)
+            usedComments.add(firstComment[:100])
             break
 
-
-    time.sleep(1800)#Tweet every 30 minutes
+    time.sleep(3)
+    #time.sleep(1800)#Tweet every 30 minutes
 
     count = count + 1
 
